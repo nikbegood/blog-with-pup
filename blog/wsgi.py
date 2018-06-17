13356177aad0741b6dac503a070618ebc2a2229a
nikbegood <nikita.tsymbalenko@mail.ru>
2018-06-17 16:36:56 +0300

    changes

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
