20e78b2971a553c5cea399541ee5fa3b1437dca7
nikbegood <nikita.tsymbalenko@mail.ru>
2018-06-17 16:11:19 +0300

    changes

--- blog/settings.py
+++ blog/settings.py
@@ -147,8 +147,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
 
 WAGTAIL_SITE_NAME = 'blog'
 
